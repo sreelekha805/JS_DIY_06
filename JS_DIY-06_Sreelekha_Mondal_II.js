69a330940741084cb8bfe65258524cb17dde7686
Sreelekha Mondal <sreelekhamondal0@gmail.com>
2025-10-15 22:40:02 +0530

    Create an input box and button. Add tasks to a list when clicked. Add a delete button for each task. Change the background color when a button is clicked.

---
+++ JS_DIY-06_Sreelekha_Mondal_II.js
@@ -0,0 +1,58 @@
+// Changing the content of the heading tag.
+document.getElementById("heading").innerHTML = "CREATE YOUR TO-DO LIST!!";
+
+// To create the to-do list this function is created.
+function addTask()
+{ 
+
+  // Accessing the input element.
+  const userInput = document.getElementById("to_do");
+
+  // Accessing the content of the input field.
+  const inputAccess = userInput.value;
+
+  // When user will not give any input, there will be an alert.
+  if (inputAccess === "")
+  {
+    alert("Please enter the valid INPUT");
+    return;
+  }
+
+  // Accessing the ul element by its id.
+  const todoList = document.getElementById("todo_list");
+
+  // Creating the element li dynamically to store the inputs as a list.
+  const todoItem = document.createElement("li");
+
+  // Adding the css class to style it. 
+  todoItem.classList.add("text");
+
+  //Adding the input given by the user in the li elements.
+  todoItem.textContent = inputAccess;
+
+  // todoItem is the child element of todoList.
+  todoList.appendChild(todoItem);
+
+  // Creating a button to delete any list item from the list.
+  const deleteItem = document.createElement("button");
+
+  // Adding the css class to style it.
+  deleteItem.classList.add("space", "button");
+
+  // Adding the text to the button element.
+  deleteItem.textContent = "DELETE";
+  todoItem.appendChild(deleteItem);
+
+  // To delete the list element.
+  deleteItem.addEventListener('click', function()
+  {
+    // removing the li element, by clicking the delete button.
+    todoItem.remove();
+  })
+}
+
+// Refresh the input field when the page is refreshed.
+window.onload = function ()
+{
+  document.getElementById ("to_do").value = '';
+}
